f7aae5835f1e30e3efa4b437fb4249fd926591d3
Faizan Rehman <faizanrehman@Faizans-MacBook-Air.local>
2018-07-26 14:32:56 +0400

    // Setup application entry point and database configuration/connectivity
    main.js // Application Entry Point
    mongoose// Use mongoose to form a application connectivity with MongoDB

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb://Camille:Camille2018@ds145881.mlab.com:45881/it_assets_directory"
+  };
+  
---
+++ main.js
@@ -0,0 +1,12 @@
+// Import required modules
+const mongoose = require("mongoose");
+
+// Get DB Key From Config File
+const db = require("./config/keys").mongoURI;
+
+//Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
